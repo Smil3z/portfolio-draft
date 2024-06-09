c0ec2eaf886c7ffb086b74c321629bff836a8114
Stephon Tonge <stephon.tonge75@gmail.com>
2024-06-09 17:58:58 -0500

    added email forms

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,31 +1,74 @@
-import { Typography } from "@mui/material";
 import React from "react";
-import Icon from "@mui/material/Icon";
+import { Box, TextField, Button, Typography } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import { Box } from "@mui/system";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
 
 function Contact() {
   return (
-    <div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "80vh",
+        backgroundColor: "black",
+      }}
+    >
       <Typography
         variant="h3"
-        sx={{ textAlign: "center", color: "white", marginTop: "50px" }}
+        sx={{ textAlign: "center", color: "white", marginBottom: "20px" }}
       >
         Let's Connect!
       </Typography>
       <Box
         sx={{
           display: "flex",
-          justifyContent: "center",
           alignItems: "center",
-          marginTop: "20px",
+          color: "white",
+          marginBottom: "15px",
         }}
       >
-        <Icon sx={{ fontSize: 150 }}>
-          <LinkedInIcon sx={{ fontSize: 150 }} />
-        </Icon>
+        <LinkedInIcon sx={{ fontSize: 150 }} />
+        <GitHubIcon sx={{ fontSize: 150 }} />
+        <EmailIcon sx={{ fontSize: 150 }} />
       </Box>
-    </div>
+      <Box
+        component="form"
+        noValidate
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2, // Adds space between the elements
+          alignItems: "center", // Center aligns the elements horizontally
+        }}
+      >
+        <TextField
+          hiddenLabel
+          id="filled-hidden-label-small-email"
+          defaultValue="Email"
+          variant="filled"
+          size="small"
+          inputProps={{ style: { color: "black", backgroundColor: "white" } }}
+          sx={{ width: "370px" }} // Adjust the width as needed
+        />
+        <TextField
+          hiddenLabel
+          id="filled-hidden-label-small-message"
+          defaultValue="Message"
+          variant="filled"
+          size="large"
+          multiline
+          rows={4} // Adjust the number of rows as needed
+          inputProps={{ style: { color: "black", backgroundColor: "white" } }}
+          sx={{ width: "380px" }} // Adjust the width as needed
+        />
+        <Button variant="contained" sx={{ width: "300px" }}>
+          Submit
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
