e49eedc5097482b2e2ce959a3cbbce0b06e23733
Stephon Tonge <stephon.tonge75@gmail.com>
2024-07-12 17:34:17 -0500

    fixed contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, TextField, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
